4db9c44d8e8c04fd6ec3b07d7367b6cc63aee45d
EC2 Default User <ec2-user@ip-172-31-48-68.ec2.internal>
2021-01-17 18:52:05 +0000

    got some code going on

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { Client, MessageEmbed } = require('discord.js');
 const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 const creds = require('./credentials.json');
 const BOT_NAME = 'nevaan-event-scheduler'
+const COMMAND_PREFIX = '!'
 
 client.once('ready', () => {
   console.log('Ready!');
@@ -13,39 +14,84 @@ client.login(creds['discordBotToken']);
 // accepted args ️(defaults in parens)
 // --date (12:15pm)
 // --title (Who wants to Pho?)
+// --mention (default 'all', { accepted 'none' })
 
 const description = 'React with a :heart_eyes: to say you are going. :frowning2: is a no (too bad for you); :thinking: is a maybe'
+const parseDate = (date = 'today') => {
+  switch (date) {
+    case 'today':
+      return new Date()
+    case 'tomorrow':
+      // code
+      return new Date()
+    case 'day-after':
+      // code
+      return new Date()
+    default:
+      const timestamp = Date.parse();
+      if (isNaN(timestamp) == false) {
+        return new Date(timestamp);
+      }
+      return new Date();
+  }
+}
+const generateOptions = (args) => {
+  const defaultDate = parseDate()
+  const defaultOptions = {
+    title: 'Who\'s down to pho?',
+    mention: 'none'
+  }
+  return {
+    title: `${defaultOptions.title} (${defaultDate})`,
+    mention: `${defaultOptions.mention}`
+  }
+}
 
 // Get cracking here
 client.on('message', (message) => {
-  if (message.content === '!pho') {
-    const embed = new MessageEmbed({
-        color: 3447003,
-        title: 'Who\'s down to pho?',
-        description,
-        fields: [
-          {
-            name: 'Going',
-            value: '----',
-            inline: true
-          },
-          {
-            name: 'Declined',
-            value: '----',
-            inline: true
-          },
-          {
-            name: 'Maybe',
-            value: '----',
-            inline: true
-          },
-        ],
-        timestamp: new Date(),
-        footer: {
-          text: 'PHODULE'
+  if (!message.content.startsWith(COMMAND_PREFIX) || message.author.bot) {
+    return
+  } else {
+    const args = message.content.slice(COMMAND_PREFIX.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+    const { title, mention, } = generateOptions(args)
+    switch (command) {
+      case 'pho':
+        const embed = new MessageEmbed({
+          color: 3447003,
+          title,
+          description,
+          fields: [
+            {
+              name: 'Going',
+              value: '----',
+              inline: true
+            },
+            {
+              name: 'Declined',
+              value: '----',
+              inline: true
+            },
+            {
+              name: 'Maybe',
+              value: '----',
+              inline: true
+            },
+          ],
+          timestamp: new Date(),
+        })
+        // Send a notification to all
+        if (mention === 'all') {
+          message.channel.send('@everyone').then(() => {
+            message.channel.send(embed)
+          })
+        } else {
+          message.channel.send(embed)
         }
-      }).setAuthor('PHODULE_BOT')
-    message.channel.send(embed);
+        break;
+      default:
+        return
+    }
   }
 });
 
