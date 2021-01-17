270926b35f636ec10b0460f609304a7efd178c70
EC2 Default User <ec2-user@ip-172-31-48-68.ec2.internal>
2021-01-17 19:59:21 +0000

    got the dates parsing

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const Discord = require('discord.js');
 const { Client, MessageEmbed } = require('discord.js');
 const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+const dayjs = require('dayjs')
 const creds = require('./credentials.json');
 const BOT_NAME = 'nevaan-event-scheduler'
 const COMMAND_PREFIX = '!'
+const ARG_PREFIX = '--'
+const ALLOWED_DATE_ARG_WORDS = new Set(['today', 'tomorrow', 'day-after'])
+const DEFAULT_HOUR = '12'
+const DEFAULT_MINUTE = '15'
 
 client.once('ready', () => {
   console.log('Ready!');
@@ -12,38 +17,90 @@ client.once('ready', () => {
 client.login(creds['discordBotToken']);
 
 // accepted args ️(defaults in parens)
-// --date (12:15pm)
+// --date (today)
+// --time (12:15pm)
 // --title (Who wants to Pho?)
 // --mention (default 'all', { accepted 'none' })
 
 const description = 'React with a :heart_eyes: to say you are going. :frowning2: is a no (too bad for you); :thinking: is a maybe'
-const parseDate = (date = 'today') => {
+const parseDate = ({ date = 'today', hour = DEFAULT_HOUR, minute = DEFAULT_MINUTE }) => {
+  const now = dayjs().format('YYYY-MM-DD')
   switch (date) {
     case 'today':
-      return new Date()
+      return dayjs(`${now}T${hour}:${minute}Z`).format('MM-DD-YYYY HH:mm A')
     case 'tomorrow':
       // code
-      return new Date()
+      return dayjs(`${now}T${hour}:${minute}Z`).add(1, 'day').format('MM-DD-YYYY HH:mm A')
     case 'day-after':
       // code
-      return new Date()
+      return dayjs(`${now}T${hour}:${minute}Z`).add(2, 'day').format('MM-DD-YYYY HH:mm A')
     default:
       const timestamp = Date.parse();
       if (isNaN(timestamp) == false) {
         return new Date(timestamp);
       }
-      return new Date();
+      return dayjs(new Date()).format('MM-DD-YYYY')
   }
 }
-const generateOptions = (args) => {
-  const defaultDate = parseDate()
-  const defaultOptions = {
-    title: 'Who\'s down to pho?',
-    mention: 'none'
-  }
+
+const isValidDate = () => {
+  return false
+}
+
+const parseArgs = (args = []) => {
+  return args.reduce((acc, curr) => {
+    if (curr.startsWith(ARG_PREFIX)) {
+      const argsSplit = curr.slice(ARG_PREFIX.length).trim().split('=');
+      if (argsSplit.length === 2) {
+        const key = argsSplit[0].toLowerCase()
+        switch (key) {
+          case 'date':
+            const dateValue = argsSplit[1].toLowerCase()
+            if (ALLOWED_DATE_ARG_WORDS.has(dateValue)) {
+              acc['date'] = dateValue
+            } else {
+              if (isValidDate(dateValue)) {
+                acc['date'] = dateValue
+              }
+            }
+            break;
+          case 'time':
+            let validHourProvided = false
+            const timeValue = argsSplit[1].toLowerCase()
+            const timesSplit = timeValue.split(':')
+            // parse the hour
+            const hourParsed = parseInt(timesSplit[0] || '0')
+            if ((hourParsed !== undefined || hourParsed !== null) && !isNaN(hourParsed) && hourParsed > -1 && hourParsed < 24) {
+              acc['hour'] = `${hourParsed < 10 ? '0' + hourParsed : hourParsed}`
+              validHourProvided = true
+            } else {
+              acc['hour'] = DEFAULT_HOUR
+            }
+            // parse the minute
+            if (validHourProvided) {
+              const minuteParsed = parseInt(timesSplit[1] || '0')
+              if ((minuteParsed !== undefined || minuteParsed !== null) && !isNaN(minuteParsed) && minuteParsed > -1 && minuteParsed < 60) {
+                acc['minute'] = `${minuteParsed < 10 ? '0' + minuteParsed : minuteParsed}`
+              } else {
+                acc['minute'] = DEFAULT_MINUTE
+              }
+            }
+            break;
+          default:
+            return acc
+        }
+      }
+    }
+    return acc
+  }, {})
+}
+
+const generateOptions = (argsParsed) => {
+  const date = parseDate({ date: 'today', ...argsParsed })
+  const options = { title: 'Who\'s down to pho?', mention: 'none', ...argsParsed }
   return {
-    title: `${defaultOptions.title} (${defaultDate})`,
-    mention: `${defaultOptions.mention}`
+    title: `${options.title} (${date})`,
+    mention: `${options.mention}`
   }
 }
 
@@ -54,7 +111,9 @@ client.on('message', (message) => {
   } else {
     const args = message.content.slice(COMMAND_PREFIX.length).trim().split(' ');
     const command = args.shift().toLowerCase();
-    const { title, mention, } = generateOptions(args)
+    const argsParsed = parseArgs(args)
+    console.log('ARGS PARSED', argsParsed)
+    const { title, mention, } = generateOptions(argsParsed)
     switch (command) {
       case 'pho':
         const embed = new MessageEmbed({
