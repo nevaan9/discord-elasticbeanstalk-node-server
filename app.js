f45028750e4fe0de29751448d6d5f224ed9f0aa2
Nevaan Perera <nevaan9@gmail.com>
2021-01-16 22:28:16 -0500

    added discord

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-const AWS = require('aws-sdk')
-const env = process.env.NODE_ENV || 'development'
-if (env === 'development') {
-    const profileCreds = require('./credentials.json')
-    const credentials = new AWS.SharedIniFileCredentials({profile: profileCreds['profile']});
-    AWS.config.credentials = credentials;
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const AWS = require("aws-sdk");
+const env = process.env.NODE_ENV || "development";
+if (env === "development") {
+  const profileCreds = require("./credentials.json");
+  const credentials = new AWS.SharedIniFileCredentials({
+    profile: profileCreds["profile"],
+  });
+  AWS.config.credentials = credentials;
 }
 
 // S3 service
-const s3 = new AWS.S3()
+const s3 = new AWS.S3();
 
-app.get('/', (req, res) => {
-  res.send(JSON.stringify({ port: port, message: 'hello from my backend server!' }))
-})
+app.get("/", (req, res) => {
+  res.send(
+    JSON.stringify({ port: port, message: "hello from my backend server!" })
+  );
+});
 
-app.get('/buckets', async (req, res) => {
-    s3.listBuckets(function(err, data) {
-        if (err) {
-            res.send(JSON.stringify(err))
-        } else {
-            res.send(JSON.stringify(data['Buckets']))
-        }
-      });
-  })
+app.get("/buckets", async (req, res) => {
+  s3.listBuckets(function (err, data) {
+    if (err) {
+      res.send(JSON.stringify(err));
+    } else {
+      res.send(JSON.stringify(data["Buckets"]));
+    }
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at ${port}`)
-})
+  console.log(`Example app listening at ${port}`);
+});
